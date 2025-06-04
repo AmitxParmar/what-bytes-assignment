79e929053a8690308d846902b7ddc121675233f3
AmitxParmar <amitparmar901@gmail.com>
2025-06-05 00:58:47 +0530

    minor changes

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,9 +2,13 @@ import { notFound } from "next/navigation";
 import { products } from "@/lib/products";
 import ClientProductPage from "@/components/ClientProductPage";
 
-type Params = Promise<{ id: string }>;
+export const dynamic = "force-dynamic";
 
-export default async function ProductPage({ params }: { params: Params }) {
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const product = products.find((p) => String(p.id) === id);
 
